d8f2fe4f8e03a4ccb4c09933c505903007900e97
Erzhan-ish <isenbekoverzan1@gmail.com>
2025-07-19 17:06:41 +0600

    hw 4 added

---
+++ lesson_4_hw.py
@@ -0,0 +1,16 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return f"Rectangle: ширина={self.width}, высота={self.height}"
+
+    def __add__(self, other):
+        return Rectangle(self.width + other.width, self.height + other.height)
+
+
+r1 = Rectangle(8, 15)
+r2 = Rectangle(6, 12)
+r3 = r1 + r2
+print(r3)
